6d030920433e8dc3a7d6a54a95cf8149df316fe2
ZvonimirZlo <lovrez2@gmail.com>
2024-08-16 21:16:39 +0200

    changed values

--- main.js
+++ main.js
@@ -6,78 +6,79 @@ const input = document.getElementById('input');
 
 
 
-const toRome = (num) => {
+const toRome = (integer) => {
     let out = '';
-    do{ if(num >= 1000){
-        out += 'M';
-        num -= 1000;
-      }else if(num >= 900 && num <= 999){
-        out += 'CM';
-        num -= 900;
-      }else if(num >= 500 && num <= 899){
-        out += 'D';
-        num -= 500;
-      }else if(num >= 400 && num <= 499){
-        out += 'CD';
-        num -= 400;
-      }else if(num >= 100 && num <= 399){
-        out += 'C';
-        num -= 100;
-      }else if(num >= 90 && num <= 99){
-        out += 'XC';
-        num -= 90;
-      }else if(num >= 50 && num <= 89){
-        out += 'L';
-        num -= 50;
-      }else if(num >= 40 && num <= 49){
-        out += 'XL';
-        num -= 40;
-      }else if(num >= 10 && num <= 39){
-        out += 'X';
-        num -= 10;
-      }else if(num > 8 && num < 10){
-        out += 'IX';
-        num -= 9;
-      }else if(num >= 5 && num <= 8){
-        out += 'V';
-        num -= 5;
-      }else if(num > 3 && num < 5){
-        out += 'IV';
-        num -= 4;
-      }else if(num <= 3){
-        out += 'I';
-        num -= 1;
-      }else{
-         return '';
+    do {
+        if (integer >= 1000) {
+            out += 'M';
+            integer -= 1000;
+        } else if (integer >= 900 && integer <= 999) {
+            out += 'CM';
+            integer -= 900;
+        } else if (integer >= 500 && integer <= 899) {
+            out += 'D';
+            integer -= 500;
+        } else if (integer >= 400 && integer <= 499) {
+            out += 'CD';
+            integer -= 400;
+        } else if (integer >= 100 && integer <= 399) {
+            out += 'C';
+            integer -= 100;
+        } else if (integer >= 90 && integer <= 99) {
+            out += 'XC';
+            integer -= 90;
+        } else if (integer >= 50 && integer <= 89) {
+            out += 'L';
+            integer -= 50;
+        } else if (integer >= 40 && integer <= 49) {
+            out += 'XL';
+            integer -= 40;
+        } else if (integer >= 10 && integer <= 39) {
+            out += 'X';
+            integer -= 10;
+        } else if (integer > 8 && integer < 10) {
+            out += 'IX';
+            integer -= 9;
+        } else if (integer >= 5 && integer <= 8) {
+            out += 'V';
+            integer -= 5;
+        } else if (integer > 3 && integer < 5) {
+            out += 'IV';
+            integer -= 4;
+        } else if (integer <= 3) {
+            out += 'I';
+            integer -= 1;
+        } else {
+            return '';
         }
-      } while(num > 0);
-      return out;
+    } while (integer > 0);
+    return out;
 };
 
-const fromRome = (str) => {
+const fromRome = (romanNum) => {
     const obj = {
-                 I: 1,
-                 IV: 4,
-                 V: 5,
-                 IX: 9,
-                 X: 10,
-                 XL: 40,
-                 L: 50,
-                 XC: 90,
-                 C: 100,
-                 CD: 400,
-                 D: 500,
-                 CM: 900,
-                 M: 1000
-                };
- 
-     return str.split('').reduce((a, b, idx) => obj[b] < obj[str[idx + 1]] ? a - obj[b] : a + obj[b], 0);
-   };
+        I: 1,
+        IV: 4,
+        V: 5,
+        IX: 9,
+        X: 10,
+        XL: 40,
+        L: 50,
+        XC: 90,
+        C: 100,
+        CD: 400,
+        D: 500,
+        CM: 900,
+        M: 1000
+    };
 
-   console.log(fromRome('MMXXIV')); //outputs 2024
-   
+    return romanNum.split('').reduce((acc, curr, idx) => obj[curr] < obj[romanNum[idx + 1]] ? acc - obj[curr] : acc + obj[curr], 0);
+};
+
+console.log(fromRome('MMXXIV')); //outputs 2024
+
 
 form.addEventListener('submit', (e) => {
- e.preventDefault();
- container.innerHTML = `${toRome(input.value)}`
-})
+    e.preventDefault();
+    container.innerHTML = `${toRome(input.value)}`
+});
